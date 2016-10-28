92b1fbf830bd02b0028e8b9778868fb5d84935cb
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-10-28 18:33:21 -0400

    coded along

--- learn-lib/interface.js
+++ learn-lib/interface.js
@@ -1,3 +1,12 @@
+// module.exports = function getUserArgument() {
+//   const userArgs = process.argv.slice(2);
+//
+//   if (userArgs.length > 1) {
+//     console.warn(`Warning: you provided more than one argument.`);
+//   }
+//
+//   return userArgs[0];
+// };
 module.exports = function getUserArgument() {
   const userArgs = process.argv.slice(2);
 
